02e31cdcbac233f84e46e4f7d8aa103cfb74c46a
rbretmounet <mrquacksxd@gmail.com>
2023-04-29 15:21:17 -0700

    Added Comments to each function

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,6 +13,15 @@ void nop(int nop_loops) {
 }
 
 int core_MHz = 0;
+
+/**
+ * Initializes the clocks of the microcontroller.
+ *
+ * This function sets up the various clock sources and frequencies for the microcontroller.
+ * It also reads the unique ID of the microcontroller and stores it in an array.
+ *
+ * @returns None
+ */
 void init_clocks() {
 	// read chip identification information. only useful when watching memory through debugger 
 	volatile int id_w_x = (*(uint32_t*)(UID_BASE) & 0xFFFF0000) >> 16;
@@ -67,6 +76,13 @@ void init_clocks() {
 	core_MHz = 80;
 }
 
+/**
+ * Initializes the GPIO pins for the microcontroller.
+ *
+ * @param None
+ *
+ * @returns None
+ */
 void init_gpio() {
 	PWR->CR2 |= 1 << 9; // enable VDDIO2 supply for GPIOG 
 	// wait until each GPIO is clocked and ready 
@@ -94,14 +110,39 @@ void init_gpio() {
 	GPIOG->MODER = 0x01445000; // leds 
 }
 
+/**
+ * Sets the specified GPIO pin to high.
+ *
+ * @param port The GPIO port to which the pin belongs.
+ * @param pin The pin number to set high.
+ *
+ * @returns None
+ */
 void gpio_high(GPIO_TypeDef * port, int pin) {
 	port->BSRR = 1 << pin;
 }
 
+/**
+ * Sets the specified GPIO pin to low logic level.
+ *
+ * @param port The GPIO port to which the pin belongs.
+ * @param pin The pin number to set to low.
+ *
+ * @returns None
+ */
 void gpio_low(GPIO_TypeDef * port, int pin) {
 	port->BSRR = 1 << (pin+16);
 }
 
+/**
+ * Sets the value of a GPIO pin on a given port.
+ *
+ * @param port The GPIO port to set the pin on.
+ * @param pin The pin number to set.
+ * @param value The value to set the pin to (1 for high, 0 for low).
+ *
+ * @returns None
+ */
 void gpio_set(GPIO_TypeDef * port, int pin, int value) {
 	if (value) {
 		gpio_high(port, pin);
@@ -110,6 +151,14 @@ void gpio_set(GPIO_TypeDef * port, int pin, int value) {
 	}
 }
 
+/**
+ * Reads the value of a GPIO pin.
+ *
+ * @param port The GPIO port to read from.
+ * @param pin The pin number to read.
+ *
+ * @returns The value of the GPIO pin (1 or 0).
+ */
 int gpio_read(GPIO_TypeDef * port, int pin) {
 	if (port->IDR & (1 << pin)) {
 		return 1;
@@ -118,30 +167,80 @@ int gpio_read(GPIO_TypeDef * port, int pin) {
 	}
 }
 
+/**
+ * Sets the state of a LED A connected to a GPIO pin.
+ *
+ * @param value The value to set the LED to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_a(int value) {
 	gpio_set(GPIOG, 11, value);
 }
 
+/**
+ * Sets the state of an LED B connected to a GPIO pin.
+ *
+ * @param value The value to set the LED to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_b(int value) {
 	gpio_set(GPIOG, 12, value);
 }
 
+/**
+ * Sets the state of an LED C connected to a GPIO pin.
+ *
+ * @param value The value to set the LED to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_c(int value) {
 	gpio_set(GPIOG, 9, value);
 }
 
+/**
+ * Sets the state of the LED fault indicator.
+ *
+ * @param value The value to set the LED fault indicator to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_fault(int value) {
 	gpio_set(GPIOG, 7, value);
 }
 
+/**
+ * Sets the state of an LED connected to a GPIO pin.
+ *
+ * @param value The value to set the LED to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_ag(int value) {
 	gpio_set(GPIOG, 6, value);
 }
 
+/**
+ * Sets the state of an Heart Beat LED connected to a GPIO pin.
+ *
+ * @param value The value to set the LED to (0 off or 1 on).
+ *
+ * @returns None
+ */
 void op_led_hb(int value) {
 	gpio_set(GPIOE, 2, value);
 }
 
+/**
+ * Sets the value of a specified LED D.
+ *
+ * @param pin The pin number to set the value for.
+ * @param value The value to set the pin to.
+ *
+ * @returns None
+ */
 void op_led_dx(int pin, int value) {
 	if (pin & 0xFFFFFF00) return; 
 	gpio_set(GPIOD, pin, value);
@@ -150,6 +249,14 @@ void op_led_dx(int pin, int value) {
 int systick_time = 0; 
 int heartbeat_counter = 0;
 int ag_counter = 0;
+/**
+ * Interrupt handler for the SysTick timer.
+ * Increments the systick_time variable and updates the status of the heartbeat and activity LEDs.
+ *
+ * @param None
+ *
+ * @returns None
+ */
 void SysTick_Handler() {
 	systick_time++;
 	
@@ -177,6 +284,14 @@ void SysTick_Handler() {
 	}
 }
 
+/**
+ * Initializes the Nested Vector Interrupt Controller (NVIC) for the SysTick timer.
+ * The SysTick timer is used for generating periodic interrupts.
+ *
+ * @param None
+ *
+ * @returns None
+ */
 void init_nvic() {
 	__disable_irq();
 	SysTick->LOAD = core_MHz/8*1000; // configure for 1 ms period, use AHB/8 
@@ -185,6 +300,16 @@ void init_nvic() {
 	__enable_irq();
 }
 
+/**
+ * Sets the line mode of a GPIO pin for SoftI2C communication.
+ *
+ * @param port The GPIO port to set the line mode for.
+ * @param pin The pin number to set the line mode for.
+ * @param deassert_line A boolean indicating whether to deassert the line or not.
+ *                      If true, the line is deasserted. If false, the line is set to output mode.
+ *
+ * @returns None
+ */
 void softi2c_line_mode(GPIO_TypeDef * port, int pin, bool deassert_line) {
 	if (deassert_line) {
 		port->MODER &= ~(0x3 << (pin*2)); 
@@ -193,6 +318,16 @@ void softi2c_line_mode(GPIO_TypeDef * port, int pin, bool deassert_line) {
 	}
 }
 
+/**
+ * Initializes the software implementation of the I2C protocol.
+ *
+ * @param scl_port The GPIO port for the SCL line.
+ * @param scl_pin The GPIO pin for the SCL line.
+ * @param sda_port The GPIO port for the SDA line.
+ * @param sda_pin The GPIO pin for the SDA line.
+ *
+ * @returns None
+ */
 void init_softi2c(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	scl_port->OTYPER = (scl_port->OTYPER & ~(1 << scl_pin)) | (0 << scl_pin); // push pull 
 	sda_port->OTYPER = (sda_port->OTYPER & ~(1 << sda_pin)) | (0 << sda_pin); 
@@ -204,10 +339,25 @@ void init_softi2c(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port,
 	softi2c_line_mode(sda_port, sda_pin, 1); 
 }
 
+/**
+ * Delays the execution of the program for a short period of time to allow for proper timing in the SoftI2C protocol.
+ *
+ * @returns None
+ */
 void softi2c_delay() {
 	nop(30);
 }
 
+/**
+ * Generates the start signal for a software-based I2C communication.
+ *
+ * @param scl_port The GPIO port for the SCL line.
+ * @param scl_pin The GPIO pin for the SCL line.
+ * @param sda_port The GPIO port for the SDA line.
+ * @param sda_pin The GPIO pin for the SDA line.
+ *
+ * @returns None
+ */
 void softi2c_sig_start(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	softi2c_line_mode(scl_port, scl_pin, 1);
 	softi2c_line_mode(sda_port, sda_pin, 1);
@@ -218,6 +368,16 @@ void softi2c_sig_start(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_
 	softi2c_delay(); 
 }
 
+/**
+ * Generates a repeated start signal for the SoftI2C protocol.
+ *
+ * @param scl_port The GPIO port for the SCL line.
+ * @param scl_pin The GPIO pin for the SCL line.
+ * @param sda_port The GPIO port for the SDA line.
+ * @param sda_pin The GPIO pin for the SDA line.
+ *
+ * @returns None
+ */
 void softi2c_sig_repeated_start(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	softi2c_line_mode(scl_port, scl_pin, 0);
 	softi2c_line_mode(sda_port, sda_pin, 1);
@@ -230,6 +390,16 @@ void softi2c_sig_repeated_start(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeD
 	softi2c_delay(); 
 }
 
+/**
+ * Sends a stop signal on the I2C bus using software implementation.
+ *
+ * @param scl_port The GPIO port of the SCL line.
+ * @param scl_pin The GPIO pin of the SCL line.
+ * @param sda_port The GPIO port of the SDA line.
+ * @param sda_pin The GPIO pin of the SDA line.
+ *
+ * @returns None
+ */
 void softi2c_sig_stop(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	softi2c_line_mode(scl_port, scl_pin, 0);
 	softi2c_line_mode(sda_port, sda_pin, 0);
@@ -240,6 +410,17 @@ void softi2c_sig_stop(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_p
 	softi2c_delay(); 
 }
 
+/**
+ * Sends an 8-bit data over a software I2C bus.
+ *
+ * @param scl_port The GPIO port of the SCL line.
+ * @param scl_pin The GPIO pin of the SCL line.
+ * @param sda_port The GPIO port of the SDA line.
+ * @param sda_pin The GPIO pin of the SDA line.
+ * @param data8 The 8-bit data to be sent.
+ *
+ * @returns None
+ */
 void softi2c_send8(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin, int data8) {
 	for (int i = 0; i < 8; i++) {
 		softi2c_line_mode(scl_port, scl_pin, 0);
@@ -251,6 +432,16 @@ void softi2c_send8(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port
 	}
 }
 
+/**
+ * Reads 8 bits of data from a SoftI2C bus.
+ *
+ * @param scl_port The GPIO port of the SCL line.
+ * @param scl_pin The GPIO pin of the SCL line.
+ * @param sda_port The GPIO port of the SDA line.
+ * @param sda_pin The GPIO pin of the SDA line.
+ *
+ * @returns The 8 bits of data read from the SoftI2C bus.
+ */
 int softi2c_read8(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	int data = 0; 
 	softi2c_line_mode(sda_port, sda_pin, 1);
@@ -265,6 +456,16 @@ int softi2c_read8(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port,
 	return data;
 }
 
+/**
+ * Sends a NACK (Not Acknowledged) signal over a software I2C bus.
+ *
+ * @param scl_port The GPIO port of the SCL line.
+ * @param scl_pin The GPIO pin of the SCL line.
+ * @param sda_port The GPIO port of the SDA line.
+ * @param sda_pin The GPIO pin of the SDA line.
+ *
+ * @returns None
+ */
 void softi2c_send_nack(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	softi2c_line_mode(scl_port, scl_pin, 0);
 	softi2c_delay();
@@ -274,6 +475,16 @@ void softi2c_send_nack(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_
 	softi2c_delay(); 
 }
 
+/**
+ * Reads a single byte from a SoftI2C device with a NACK signal.
+ *
+ * @param scl_port The GPIO port of the SCL line.
+ * @param scl_pin The GPIO pin of the SCL line.
+ * @param sda_port The GPIO port of the SDA line.
+ * @param sda_pin The GPIO pin of the SDA line.
+ *
+ * @returns The byte read from the SoftI2C device.
+ */
 int softi2c_read_nack(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin) {
 	softi2c_line_mode(scl_port, scl_pin, 0);
 	softi2c_delay();
@@ -286,6 +497,19 @@ int softi2c_read_nack(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_p
 	return nack; 
 }
 
+/**
+ * Writes data to a register of a device using software I2C protocol.
+ *
+ * @param scl_port The GPIO port of the SCL pin.
+ * @param scl_pin The pin number of the SCL pin.
+ * @param sda_port The GPIO port of the SDA pin.
+ * @param sda_pin The pin number of the SDA pin.
+ * @param device_addr The address of the device to write to.
+ * @param reg_addr The address of the register to write to.
+ * @param data The data to write to the register.
+ *
+ * @returns The number of NACKs received during the write operation.
+ */
 int softi2c_write_reg(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin, int device_addr, int reg_addr, int data) {
 	int nack = 0; 
 	softi2c_sig_start(scl_port, scl_pin, sda_port, sda_pin);
@@ -299,6 +523,18 @@ int softi2c_write_reg(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_p
 	return nack; 
 }
 
+/**
+ * Reads a register from a device using software I2C protocol.
+ *
+ * @param scl_port The GPIO port of the SCL pin.
+ * @param scl_pin The GPIO pin number of the SCL pin.
+ * @param sda_port The GPIO port of the SDA pin.
+ * @param sda_pin The GPIO pin number of the SDA pin.
+ * @param device_addr The address of the device to read from.
+ * @param reg_addr The address of the register to read.
+ *
+ * @returns The value of the register read from the device.
+ */
 int softi2c_read_reg(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin, int device_addr, int reg_addr) {
 	int nack = 0; 
 	softi2c_sig_start(scl_port, scl_pin, sda_port, sda_pin);
@@ -315,6 +551,17 @@ int softi2c_read_reg(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_po
 	return data;
 }
 
+/**
+ * Probes a device on the I2C bus using software I2C.
+ *
+ * @param scl_port The GPIO port for the SCL pin.
+ * @param scl_pin The GPIO pin for the SCL pin.
+ * @param sda_port The GPIO port for the SDA pin.
+ * @param sda_pin The GPIO pin for the SDA pin.
+ * @param device_addr The address of the device to probe.
+ *
+ * @returns True if the device is present on the bus, false otherwise.
+ */
 bool softi2c_probe(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin, int device_addr) {
 	int nack = 0; 
 	softi2c_sig_start(scl_port, scl_pin, sda_port, sda_pin);
@@ -328,10 +575,30 @@ bool softi2c_probe(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port
 	}
 }
 
+/**
+ * Reads a 16-bit register value from a device using software I2C protocol.
+ *
+ * @param scl_port The GPIO port of the SCL pin.
+ * @param scl_pin The pin number of the SCL pin.
+ * @param sda_port The GPIO port of the SDA pin.
+ * @param sda_pin The pin number of the SDA pin.
+ * @param device_addr The address of the device to read from.
+ * @param high_reg_addr The address of the high byte of the register to read.
+ * @param low_reg_addr The address of the low byte of the register to read.
+ *
+ * @returns The 16-bit register value read from the device.
+ */
 int16_t softi2c_read_reg_hl(GPIO_TypeDef * scl_port, int scl_pin, GPIO_TypeDef * sda_port, int sda_pin, int device_addr, int high_reg_addr, int low_reg_addr) {
 	return softi2c_read_reg(scl_port, scl_pin, sda_port, sda_pin, device_addr, high_reg_addr) << 8 | softi2c_read_reg(scl_port, scl_pin, sda_port, sda_pin, device_addr, low_reg_addr);
 }
 
+/**
+ * Initializes the Analog-to-Digital Converter (ADC) by configuring the GPIO pin for analog input.
+ *
+ * @param None
+ *
+ * @returns None
+ */
 void init_adc() {
 	GPIOA->MODER |= 0x3 << (1*2);
 	
@@ -375,6 +642,15 @@ void init_adc() {
 #define MAG_OVERSAMPLE_128 	2 
 #define MAG_OVERSAMPLE_64  	3 
 
+/**
+ * Configures the accelerometer control register of an IMU device.
+ *
+ * @param acel_rate The rate of the accelerometer.
+ * @param acel_scale The scale of the accelerometer.
+ * @param digital_filter_on Whether the digital filter is enabled or not.
+ *
+ * @returns None
+ */
 void op1_imu_acel_ctrl(int acel_rate, int acel_scale, int digital_filter_on) {
 	acel_rate &= 0xFF;
 	acel_scale &= 0xF;
@@ -383,6 +659,14 @@ void op1_imu_acel_ctrl(int acel_rate, int acel_scale, int digital_filter_on) {
 	softi2c_write_reg(OP1_I2C2, IMU_ADDR, 0x10, data); 
 }
 
+/**
+ * Configures the gyroscope rate and scale for the IMU sensor.
+ *
+ * @param gyro_rate The rate of the gyroscope.
+ * @param gyro_scale The scale of the gyroscope.
+ *
+ * @returns None
+ */
 void op1_imu_gyro_ctrl(int gyro_rate, int gyro_scale) {
 	gyro_rate &= 0xFF;
 	gyro_scale &= 0xFF; 
@@ -390,6 +674,16 @@ void op1_imu_gyro_ctrl(int gyro_rate, int gyro_scale) {
 	softi2c_write_reg(OP1_I2C2, IMU_ADDR, 0x11, data); 
 }
 
+/**
+ * Initializes the OP1 IMU with the given accelerometer and gyroscope settings.
+ *
+ * @param acel_rate The accelerometer data rate.
+ * @param acel_scale The accelerometer full scale range.
+ * @param gyro_rate The gyroscope data rate.
+ * @param gyro_scale The gyroscope full scale range.
+ *
+ * @returns None
+ */
 void op1_imu_init(int acel_rate, int acel_scale, int gyro_rate, int gyro_scale) {
 	softi2c_write_reg(OP1_I2C2, IMU_ADDR, 0x12, 0x01); // soft reset imu  
 	nop(100); 
@@ -397,34 +691,78 @@ void op1_imu_init(int acel_rate, int acel_scale, int gyro_rate, int gyro_scale) 
 	op1_imu_gyro_ctrl(gyro_rate, gyro_scale);
 } 
 
+/**
+ * Reads the X-axis acceleration value from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The X-axis acceleration value as a 16-bit signed integer.
+ */
 int16_t op1_imu_read_acel_x() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x29, 0x28);
 }
 
+/**
+ * Reads the y-axis acceleration value from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The y-axis acceleration value as a 16-bit signed integer.
+ */
 int16_t op1_imu_read_acel_y() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x2B, 0x2A);
 }
 
+/**
+ * Reads the acceleration value of the z-axis from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The acceleration value of the z-axis.
+ */
 int16_t op1_imu_read_acel_z() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x2D, 0x2C);
 }
 
+/**
+ * Reads the X-axis gyroscope data from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The X-axis gyroscope data as a 16-bit signed integer.
+ */
 int16_t op1_imu_read_gyro_x() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x23, 0x22);
 }
 
+/**
+ * Reads the y-axis gyroscope value from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The y-axis gyroscope value as a 16-bit signed integer.
+ */
 int16_t op1_imu_read_gyro_y() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x25, 0x24);
 }
 
+/**
+ * Reads the Z-axis gyroscope data from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The Z-axis gyroscope data as a 16-bit signed integer.
+ */
 int16_t op1_imu_read_gyro_z() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x27, 0x26);
 }
 
+/**
+ * Reads the temperature from the IMU sensor connected to the I2C2 bus of OP1.
+ *
+ * @returns The temperature value in 16-bit signed integer format.
+ */
 int16_t op1_imu_read_temp() {
 	return softi2c_read_reg_hl(OP1_I2C2, IMU_ADDR, 0x21, 0x20);
 }
 
+/**
+ * Configures the control registers of the OP1 magnetometer.
+ *
+ * @param rate The output data rate of the magnetometer.
+ * @param scale The full scale range of the magnetometer.
+ * @param oversample The number of samples to average for each measurement.
+ *
+ * @returns None
+ */
 void op1_mag_ctrl(int rate, int scale, int oversample) {
 	if (rate) {
 		softi2c_write_reg(OP1_I2C2, MAG_ADDR, 0x09, oversample << 6 | scale << 4 | (rate-1) << 2 | 1); 
@@ -433,6 +771,15 @@ void op1_mag_ctrl(int rate, int scale, int oversample) {
 	}
 }
 
+/**
+ * Initializes the magnetometer for operation.
+ *
+ * @param rate The data output rate of the magnetometer.
+ * @param scale The full-scale range of the magnetometer.
+ * @param oversample The oversampling rate of the magnetometer.
+ *
+ * @returns None
+ */
 void op1_mag_init(int rate, int scale, int oversample) {
 	softi2c_write_reg(OP1_I2C2, MAG_ADDR, 0x0A, 0x80); // soft reset 
 	nop(50); 
@@ -442,22 +789,49 @@ void op1_mag_init(int rate, int scale, int oversample) {
 	op1_mag_ctrl(rate, scale, oversample);
 }
 
+/**
+ * Reads the x-axis magnetometer value from the OP1 sensor.
+ *
+ * @returns The x-axis magnetometer value.
+ */
 int16_t op1_mag_read_x() {
 	return softi2c_read_reg_hl(OP1_I2C2, MAG_ADDR, 0x01, 0x00);
 }
 
+/**
+ * Reads the Y-axis magnetometer value from the OP1 sensor.
+ *
+ * @returns The Y-axis magnetometer value as a 16-bit integer.
+ */
 int16_t op1_mag_read_y() {
 	return softi2c_read_reg_hl(OP1_I2C2, MAG_ADDR, 0x03, 0x02);
 }
 
+/**
+ * Reads the magnitude value of the z-axis from the magnetometer connected to the I2C2 bus of OP1.
+ *
+ * @returns The magnitude value of the z-axis.
+ */
 int16_t op1_mag_read_z() {
 	return softi2c_read_reg_hl(OP1_I2C2, MAG_ADDR, 0x05, 0x04);
 }
 
+/**
+ * Reads the temperature from the magnetometer connected to OP1 via I2C2.
+ *
+ * @returns The temperature value in 16-bit signed integer format.
+ */
 int16_t op1_mag_read_temp() {
 	return softi2c_read_reg_hl(OP1_I2C2, MAG_ADDR, 0x08, 0x07);
 }
 
+/**
+ * Computes the absolute value of a 16-bit integer.
+ *
+ * @param a The input 16-bit integer.
+ *
+ * @returns The absolute value of the input integer.
+ */
 unsigned int abs16(int16_t a) {
 	if (a < 0) {
 		return (a * -1);
